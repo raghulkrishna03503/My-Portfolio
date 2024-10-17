900df2999dbc3806cdd56f906c036d4aff8d0ad5
raghulkrishna03503 <raghulkrishna03052003@gmail.com>
2024-10-17 09:45:06 +0530

    Third commit

--- src/Education.js
+++ src/Education.js
@@ -10,18 +10,37 @@ const Education = () => {
                 <div className="education-cards">
                     <div className="education-card">
                         <div className="institution">
-                            <h3>Massachusetts Institute of Technology (MIT)</h3>
-                            <p>Bachelor of Science in Computer Science and Engineering</p>
-                            <p><strong>Year:</strong> 2016 - 2020</p>
-                            <p><strong>Grade:</strong> 3.8 GPA</p>
+                            <h3> Sri Sivasubramaniya Nadar College of Engineering</h3>
+                            <p>B.E. Computer Science Engineering</p>
+                            <p><strong>Year: </strong>2021 - 2025</p>
+                            <p><strong>CGPA: </strong>8.18</p>
                         </div>
                     </div>
                     <div className="education-card">
                         <div className="institution">
-                            <h3>Stanford University</h3>
-                            <p>Master of Science in Artificial Intelligence</p>
-                            <p><strong>Year:</strong> 2020 - 2022</p>
-                            <p><strong>Grade:</strong> 4.0 GPA</p>
+                            <h3> Indian Institute of Technology Madras</h3>
+                            <p>B.S. Data Science</p>
+                            <p><strong>Year: </strong>2022 - 2025</p>
+                            <p><strong>CGPA: </strong>7.63</p>
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+                <div className="education-cards">
+                    <div className="education-card">
+                        <div className="institution">
+                            <h3>Sri Ram Dayal Khemka Vivekananda Vidyalaya Senior Secondary School</h3>
+                            <p>Class 12</p>
+                            <p><strong>Year: </strong>2020 - 2021</p>
+                            <p><strong>Percentage: </strong>95.6% - AISSCE</p>
+                        </div>
+                    </div>
+                    <div className="education-card">
+                        <div className="institution">
+                            <h3>Sri Ram Dayal Khemka Vivekananda Vidyalaya Senior Secondary School</h3>
+                            <p>Class 10</p>
+                            <p><strong>Year: </strong>2018 - 2019</p>
+                            <p><strong>Percentage: </strong>93.4% - AISSE</p>
                         </div>
                     </div>
                 </div>
--- src/Experience.js
+++ src/Experience.js
@@ -9,30 +9,13 @@ const Experience = () => {
         <h2><b><i>Experience</i></b></h2>
         <div className="experience-cards">
           <div className="experience-card facebook">
-            <h3>Facebook</h3>
-            <h4>Software Engineer</h4>
-            <p>June 2018 - Present</p>
-            <ul>
-              <li>Worked on various projects using React and Node.js.</li>
-              <li>Implemented new features and optimized existing code.</li>
-            </ul>
-          </div>
-          <div className="experience-card quora">
-            <h3>Quora</h3>
-            <h4>Front-End Developer</h4>
-            <p>May 2017 - May 2018</p>
-            <ul>
-              <li>Developed interactive UI components using Angular.</li>
-              <li>Collaborated with the backend team to integrate APIs.</li>
-            </ul>
-          </div>
-          <div className="experience-card airbnb">
-            <h3>Airbnb</h3>
+            <h3>Fidelity Investments</h3>
             <h4>Software Engineer Intern</h4>
-            <p>Jan 2015 - Sep 2015</p>
+            <p>June 2024 - August 2024</p>
             <ul>
-              <li>Assisted in developing features for the Airbnb web app.</li>
-              <li>Worked with a team to improve application performance.</li>
+              {/* <li>During this Internship, I worked on developing a Data Catalog of various fidelity funds for Business operation team of<br></br>Fidelity
+Investments.
+</li> */}
             </ul>
           </div>
         </div>
